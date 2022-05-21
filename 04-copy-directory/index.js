8d866b25091940d5a25b146cb6b1424058d7ebbf
Roman Putilov <Putilov.Roman@gmail.com>
2022-05-21 09:09:38 +0500

    feat: add 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const path = require('path');
+const { mkdir, readdir, copyFile, constants } = require('fs');
 
+mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, () => {
+  readdir(path.join(__dirname, 'files'), (_, files) => {
+    for (const file of files) {
+      copyFile(
+        path.join(__dirname, 'files', file),
+        path.join(__dirname, 'files-copy', file),
+        constants.COPYFILE_FICLONE,
+        () => {}
+      );
+    }
+  });
+});
