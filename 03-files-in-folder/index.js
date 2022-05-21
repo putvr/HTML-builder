a0ff4b469a9265ee2a7c9026024222abc49c488c
Roman Putilov <Putilov.Roman@gmail.com>
2022-05-21 08:56:58 +0500

    feat: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const {readdir, stat } = require('fs');
+
+const path = require('path');
+
+readdir(path.join(__dirname,  'secret-folder'), (err, files) => {
+  for (const file of files) {   
+    const {name, ext } = path.parse(file);
+
+    stat(path.join(__dirname,  'secret-folder', file), (_, stats) => {
+      if(!stats.isFile()) return;
+
+      const size = stats.size;
+      console.log(`${name} - ${ext.slice(1)} - ${size}b`);
+    });
+  }
+});
